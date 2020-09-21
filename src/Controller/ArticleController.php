bcdfeda36658ae79c681baf003304a819cd84f37
Pierre Bretéché <pbreteche@dawan.fr>
2020-09-21 15:51:40 +0200

    find 1 post API

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -17,4 +17,18 @@ class ArticleController extends AbstractController
 
         return $this->json($articles);
     }
+
+    /**
+     * @Route("/article/{id}", requirements={"id": "\d+"})
+     */
+    public function show(int $id, PostRepository $repository)
+    {
+        $article = $repository->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException('Article '.$id.' not found.');
+        }
+
+        return $this->json($article);
+    }
 }

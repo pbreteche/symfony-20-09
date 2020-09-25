5c09d5524b94dcb194e8a45bffd858b99fb00cdb
Pierre Bretéché <pbreteche@dawan.fr>
2020-09-25 10:44:18 +0200

    inject dependency

--- src/Math/Adder.php
+++ src/Math/Adder.php
@@ -2,11 +2,24 @@
 
 namespace App\Math;
 
+use Psr\Log\LoggerInterface;
+
 class Adder
 {
 
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function calculate(int $a, int $b)
     {
+        $this->logger->debug('Addition de '.$a.' et '.$b);
         return $a + $b;
     }
 }

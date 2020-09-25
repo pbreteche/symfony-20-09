23e7033fbe17cd953bd5ff6a6cd2f477248a704f
Pierre Bretéché <pbreteche@dawan.fr>
2020-09-25 10:54:16 +0200

    inject scalar dependency

--- src/Math/Adder.php
+++ src/Math/Adder.php
@@ -11,15 +11,20 @@ class Adder
      * @var \Psr\Log\LoggerInterface
      */
     private $logger;
+    /**
+     * @var string
+     */
+    private $unit;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, string $unit)
     {
         $this->logger = $logger;
+        $this->unit = $unit;
     }
 
     public function calculate(int $a, int $b)
     {
         $this->logger->debug('Addition de '.$a.' et '.$b);
-        return $a + $b;
+        return ($a + $b).$this->unit;
     }
 }
